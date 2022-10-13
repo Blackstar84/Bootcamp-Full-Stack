6479568a2f2c425b6b32815e12d7dab94d496da0
Blackstar84 <carlosalmada84@gmail.com>
2022-10-13 08:55:58 -0300

    se agrega el video 2 semana 1 javascript

---
+++ Desarrollo WEB Frontend/JS/Semana 1 video 2/assets/js/ejercicio1.js
@@ -0,0 +1,36 @@
+
+let ejercicioTitle = "<h2> Preguntas y Respuestas desde JS</h2>";
+
+console.log(ejercicioTitle);
+
+document.write(ejercicioTitle)
+
+let capitalPeru, capitalChile, capitalColombia;
+
+// capitalPeru = window.prompt("¿Cual es la capital de Peru?");
+
+// capitalPeru = "<p>" + capitalPeru + "</p>"
+// document.write(capitalPeru);
+
+
+// capitalChile = window.prompt("¿Cual es la capital de Chile?");
+// capitalChile = "<p>" + capitalChile + "</p>"
+// document.write(capitalChile);
+
+
+// capitalColombia = window.prompt("¿Cual es la capital de Colombia?");
+
+// capitalColombia = "<p>" + capitalColombia + "</p>"
+// document.write(capitalColombia);
+
+
+
+// let CountpaisesLatam;
+// CountpaisesLatam = window.prompt("¿Cuantos paises tiene latoinoAmerica");
+// document.write(CountpaisesLatam * 2);
+
+
+let juegoFab;
+juegoFab = window.prompt("¿Cual es tu juego favorito?");
+
+document.getElementById("caja_juegos").innerHTML = juegoFab;
