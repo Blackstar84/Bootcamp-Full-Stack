5b0b931b05f6374579fa1088c27709c0e35903e4
Blackstar84 <carlosalmada84@gmail.com>
2023-01-20 17:04:20 -0300

    se agrega el color y estilo al button del último cards read the docs

--- Desarrollo WEB Frontend/React/bootstrap/src/components/Cards.js
+++ Desarrollo WEB Frontend/React/bootstrap/src/components/Cards.js
@@ -47,7 +47,7 @@ function BasicExample() {
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button style={{backgroundColor:"#6528E0", padding:"20px"}}>Read the docs</Button>
             </Card.Body>
         </Card>
     </div>
